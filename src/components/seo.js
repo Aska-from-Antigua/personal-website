a095ecf555193846e18f0743d4c054ff1a39ecfc
Jerry Aska <krisaska1412@gmail.com>
2025-11-16 15:36:12 -0500

    Add Simple Analytics for privacy-friendly visitor tracking

--- src/components/seo.js
+++ src/components/seo.js
@@ -49,6 +49,9 @@ const Seo = ({ title, description, image, url }) => {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+
+      {/* Simple Analytics - 100% privacy-first analytics */}
+      <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
     </Helmet>
   )
 }
